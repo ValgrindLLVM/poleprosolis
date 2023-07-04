afdedbe061573e22a90603df8326acb733f809d2
ValgrindLLVM <valgrindllvm@proton.me>
2023-07-04 16:13:43 +0300

    [ppl-game] assets/maps/farm: update to .into()
    - change Block::Foo(x) to x.into()

--- ppl-game/src/assets/maps/farm.rs
+++ ppl-game/src/assets/maps/farm.rs
@@ -1,5 +1,5 @@
 use crate::{
-    assets::blocks::{generic::Generic, wheat::Wheat, Block},
+    assets::blocks::{generic::Generic, wheat::Wheat},
     map::{BlockData, CollisionTy},
     ui::{BlockTy, Point},
 };
@@ -7,16 +7,16 @@ use crate::{
 #[rustfmt::skip]
 pub fn init() -> Vec<BlockData> {
     vec![
-        BlockData::new(Point(1, 1), CollisionTy::CanUse, BlockTy::Wheat, Block::Wheat(Wheat::new())),
-        BlockData::new(Point(2, 1), CollisionTy::CanUse, BlockTy::Wheat, Block::Wheat(Wheat::new())),
-        BlockData::new(Point(3, 1), CollisionTy::CanUse, BlockTy::Wheat, Block::Wheat(Wheat::new())),
-        BlockData::new(Point(4, 1), CollisionTy::CanUse, BlockTy::Wheat, Block::Wheat(Wheat::new())),
-        BlockData::new(Point(5, 1), CollisionTy::CanUse, BlockTy::Wheat, Block::Wheat(Wheat::new())),
+        BlockData::new(Point(1, 1), CollisionTy::CanUse, BlockTy::Wheat, Wheat::new().into()),
+        BlockData::new(Point(2, 1), CollisionTy::CanUse, BlockTy::Wheat, Wheat::new().into()),
+        BlockData::new(Point(3, 1), CollisionTy::CanUse, BlockTy::Wheat, Wheat::new().into()),
+        BlockData::new(Point(4, 1), CollisionTy::CanUse, BlockTy::Wheat, Wheat::new().into()),
+        BlockData::new(Point(5, 1), CollisionTy::CanUse, BlockTy::Wheat, Wheat::new().into()),
 
-        BlockData::new(Point(1, 2), CollisionTy::Collision, BlockTy::Water, Block::Generic(Generic)),
-        BlockData::new(Point(2, 2), CollisionTy::Collision, BlockTy::Water, Block::Generic(Generic)),
-        BlockData::new(Point(3, 2), CollisionTy::Collision, BlockTy::Water, Block::Generic(Generic)),
-        BlockData::new(Point(4, 2), CollisionTy::Collision, BlockTy::Water, Block::Generic(Generic)),
-        BlockData::new(Point(5, 2), CollisionTy::Collision, BlockTy::Water, Block::Generic(Generic)),
+        BlockData::new(Point(1, 2), CollisionTy::Collision, BlockTy::Water, Generic.into()),
+        BlockData::new(Point(2, 2), CollisionTy::Collision, BlockTy::Water, Generic.into()),
+        BlockData::new(Point(3, 2), CollisionTy::Collision, BlockTy::Water, Generic.into()),
+        BlockData::new(Point(4, 2), CollisionTy::Collision, BlockTy::Water, Generic.into()),
+        BlockData::new(Point(5, 2), CollisionTy::Collision, BlockTy::Water, Generic.into()),
     ]
 }
